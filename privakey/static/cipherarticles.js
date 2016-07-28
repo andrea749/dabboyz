b997afc1e0a6e9e84a351b9c664dac9431a400fc
Aureliano Yepez <yepez4845@gmail.com>
2016-07-28 15:20:39 -0700

    update

---
+++ privakey/static/cipherarticles.js
@@ -0,0 +1,15 @@
+$(function(){
+		$("button").hover(function(){
+				$(this).css("text-shadow", "0 0 20px #ff0000, 0 0 20px #ff000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px");
+			}, function(){
+				$(this).css("text-shadow", "0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0F0, 0 0 20px");
+		});
+	});
+
+  $(function(){
+  		$("h1").hover(function(){
+  				$(this).css("text-shadow","0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px");
+  			}, function(){
+  				$(this).css("text-shadow","0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px");
+  		});
+  	});
---
+++ privakey/static/homepage.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("button").hover(function(){
+        $(this).css("text-shadow","0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px");
+      }, function(){
+        $(this).css("text-shadow","0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px");
+    });
+  });
