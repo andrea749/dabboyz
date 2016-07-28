53ea07ae343315a52f7de3113fa4c050febc4ed3
Aureliano Yepez <yepez4845@gmail.com>
2016-07-28 11:23:48 -0700

    update

--- privakey/static/cycle.js
+++ privakey/static/cycle.js
@@ -1,3 +1,15 @@
-$('#image').click(function() {
-  $(this).slideUp(1000)
-});
+$(function(){
+		$("button").hover(function(){
+				$(this).css("text-shadow", "0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px");
+			}, function(){
+				$(this).css("text-shadow", "0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px");
+		});
+	});
+
+  $(function(){
+  		$("h1").hover(function(){
+  				$(this).css("text-shadow","0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px #0000ff, 0 0 20px");
+  			}, function(){
+  				$(this).css("text-shadow","0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px #ff0000, 0 0 20px");
+  		});
+  	});
